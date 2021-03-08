00a057e8d2c5397464332636fb71e2540d3ee434
Pavel Pugachev <pavel.pugachev@juriba.com>
2021-03-08 18:22:59 +0200

    commit2

--- src/test/java/RozetkaSearchTest.java
+++ src/test/java/RozetkaSearchTest.java
@@ -12,13 +12,13 @@ public class RozetkaSearchTest {
     public void arrange () {
         System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
         webDriver = new ChromeDriver();
-        SearchPage searchPage = new SearchPage(webDriver, 5);
+        SearchPage searchPage = new SearchPage(webDriver, 10);
         searchPage.navigateToTestLocation();
     }
 
     @Test(dataProvider = "searchInputProvider", dataProviderClass = SearchInputProvider.class)
     public void act (String data) {
-        SearchPage searchPage = new SearchPage(webDriver, 5);
+        SearchPage searchPage = new SearchPage(webDriver, 10);
         searchPage.findSearchField().search(data).check();
     }
 
