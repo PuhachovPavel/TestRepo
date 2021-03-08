1582b631ceb3ec8922bf064881db27c5d29a3e2f
Pavel Pugachev <pavel.pugachev@juriba.com>
2021-03-08 18:41:14 +0200

    commit5

--- src/test/java/SearchPage.java
+++ src/test/java/SearchPage.java
@@ -26,11 +26,6 @@ public class SearchPage {
     }
 
     public SearchPage findSearchField () {
-        try {
-            Thread.sleep(5000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
         webDriver.manage().timeouts().implicitlyWait(timeOut, TimeUnit.SECONDS);
         searchField = webDriver.findElement(By.xpath("//app-root/div/div[1]/rz-header/header/div/div/div/form/div/div/input"));
         return this;
@@ -47,11 +42,6 @@ public class SearchPage {
     public void check () {
         webDriver.manage().timeouts().implicitlyWait(timeOut, TimeUnit.SECONDS);
         WebElement allResultsTab = webDriver.findElement(By.xpath("//rz-search/rz-catalog/div/div[2]/aside/rz-filter-stack/div[1]/div/div/rz-filter-categories/ul/li[1]/a/span[1]"));
-        try {
-            Thread.sleep(5000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
         assert (allResultsTab.isDisplayed());
         WebElement clearSearchFieldButton = webDriver.findElement(By.xpath("/html/body/app-root/div/div[1]/rz-header/header/div/div/div/form/div/div/button[2]"));
         clearSearchFieldButton.click();
